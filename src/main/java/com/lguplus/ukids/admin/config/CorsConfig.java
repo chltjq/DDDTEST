f8aefe0d5a0c3020cc59b2f61e6590dc74b16661
cjs <cjs_93@naver.com>
2021-11-11 11:36:58 +0900

    /*****/

--- src/main/java/com/lguplus/ukids/admin/config/CorsConfig.java
+++ src/main/java/com/lguplus/ukids/admin/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
+	/******/
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS")
